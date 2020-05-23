ee512ee54991a6a99cf28c3057c5c04ed4177cca
arleyribeiro <arley.sribeiro@gmail.com>
2020-05-23 19:20:32 -0300

    Fix a bug and method for remove a element

--- theory_data_structures/LinkedList/singly_linked_list/singlyLinkedList.cpp
+++ theory_data_structures/LinkedList/singly_linked_list/singlyLinkedList.cpp
@@ -8,16 +8,16 @@ struct node {
 };
 
 class List {
-    node * head = NULL;
+  node * head = new node;
   public:
     List() {
-      
+      head->next = NULL;
     };
     bool isEmpty() {
-      return head == NULL;
+      return head->next == NULL;
     }
     void insert(int data);
-    //void remove(int data);
+    void remove(int data);
     void show();
 };
 
@@ -26,11 +26,11 @@ void List::insert(int data) {
   no->data = data;
   no->next = NULL;
   if (isEmpty()) {
-    head = no;
+    head->next = no;
     return;
   }
   node *parent;
-  node *ptr = head;
+  node *ptr = head->next;
   while(ptr != NULL) {
     parent = ptr;
     ptr = ptr->next;
@@ -42,9 +42,33 @@ void List::show() {
   if (isEmpty()) {
     return;
   }
-  node *ptr = head;
+  node *ptr = head->next;
   while(ptr != NULL) {
     cout << ptr->data << " ";
+    ptr = ptr->next;
+  }
+  cout << endl;
+}
+
+void List::remove(int data) {
+  if (isEmpty()) {
+    return;
+  }
+  node *parent = head;
+  node *ptr = head->next;
+  while(ptr != NULL) {
+    if (ptr->data == data) {
+      node *next = new node;      
+      if (ptr->next != NULL) {
+        next = ptr->next;
+        parent->next = next;
+      } else {
+        parent->next = NULL;
+      }
+      free(ptr);
+      return;
+    }
+    parent = ptr;
     ptr = ptr->next;
   }
   cout << endl;
@@ -57,6 +81,20 @@ int main () {
   list.insert(2);
   list.insert(3);
   list.insert(4);
+  list.insert(5);
+  list.insert(6);
+  list.insert(7);
+  list.insert(8);
+  list.insert(9);
+  list.insert(10);
+  list.show();
+  list.remove(9);
+  list.show();
+  list.remove(5);
+  list.show();
+  list.remove(10);
+  list.show();
+  list.remove(1);
   list.show();
 
   return 0;
