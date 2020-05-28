88fca21b620923bdacad4476252f657f2a492933
arleyribeiro <arley.sribeiro@gmail.com>
2020-05-28 19:58:14 -0300

    start method to remove by index

--- theory_data_structures/linked_list/double_linked_list/dobble_linked_list.cpp
+++ theory_data_structures/linked_list/double_linked_list/dobble_linked_list.cpp
@@ -23,6 +23,7 @@ class List {
     void insert(int data);
     void show(bool inverseOrder);
     void remove(int data);
+    void removeByIndex(int index);
     int size();
 };
 
@@ -70,8 +71,47 @@ void List::remove(int data) {
   }
 }
 
+void List::removeByIndex(int index) {
+  
+  int count = 0;
+  if (index < 0 || (index > this->size()-1) )
+  {
+    cout << "Index out of range" << endl;
+    return;
+  }
+  node *ptr = head->next;
+  node *parent = head;
+  while (ptr != nullptr)
+  {
+    if (count == index)
+    {
+      if (index == 0 && ptr->next != nullptr)
+      {
+        head->next = ptr->next;
+        ptr->next->previous = head;
+      } else {
+        parent->next = ptr->next;
+        ptr->next->previous = parent;
+      }
+      free(ptr);
+      break;
+    }
+    else if (ptr->next == nullptr && (count + 1) == index)
+    {
+      parent->next = NULL;
+      free(ptr);
+      break;
+    }
+    count++;
+    parent = ptr;
+    ptr = ptr->next;
+  }
+}
+
 void List::show(bool inverseOrder) {
   if (isEmpty()) {
+    head->next = NULL;
+    head->previous = NULL;
     cout << "Empty list!" << endl;
     return;
   }
@@ -120,5 +160,31 @@ int main() {
   list.remove(4);
   list.show(true);
   cout << list.size() << endl;
+
+  list.insert(1);
+  list.insert(2);
+  list.insert(3);
+  list.insert(4);
+  list.insert(5);
+  cout << list.size() << endl;
+  list.removeByIndex(4);
+  list.removeByIndex(3);
+  list.removeByIndex(2);
+  list.removeByIndex(1);
+  list.removeByIndex(0);
+
+  list.insert(1);
+  list.insert(2);
+  list.insert(3);
+  list.insert(4);
+  list.insert(5);
+  list.show(true);
+
+  list.removeByIndex(0);
+  list.removeByIndex(1);
+  list.removeByIndex(2);
+  list.removeByIndex(1);
+  list.removeByIndex(0);
+  list.show(true);
   return 0;
 }
