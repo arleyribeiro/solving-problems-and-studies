8c86b5198d59a79e02ac62c6ea81abfb417497e1
Arley Ribeiro <arley.sribeiro@gmail.com>
2020-06-02 10:16:09 -0300

    FindTwoSumFaster - complexity O(n)

--- c#/Program.cs
+++ c#/Program.cs
@@ -26,6 +26,30 @@ namespace Program
             }
             return null;
         }
+        
+        public static Tuple<int, int> FindTwoSumFaster(IList<int> list, int sum)
+        {
+            if (list == null)
+                return null;
+
+            var listSet = new Dictionary<int, int>();
+            int i = 0;
+            // O(n)
+            foreach (var elem in list)
+            {
+                if (listSet.Count > 0) {
+                    var search = sum - elem;
+                    if (listSet.ContainsKey(search)) {
+                        var index = listSet[search];
+                        if (i != index) {
+                            return new Tuple<int, int>(i, index);
+                        }
+                    }
+                }
+                listSet[elem] = i++;
+            }
+            return null;
+        }
         static void Main(string[] args)
         {          
             var list = new List<int>() { 3, 1, 5, 7, 5, 9, 4, 8,3,47,85,9,6,25,96,9,332,7,89,62,74,1,78,3,9,3,4,9, 4,6,12,13,11,18,90 };
