47dc56574b44a2aef9b6b31ebde2c467ae8e70fb
Arley Ribeiro <arley.sribeiro@gmail.com>
2020-06-01 23:46:11 -0300

    Fixed the logic, improve to performance

--- c#/Program.cs
+++ c#/Program.cs
@@ -7,30 +7,24 @@ namespace Program
     {
         public static Tuple<int, int> FindTwoSum(IList<int> list, int target)
         {
-            Dictionary<int, List<Tuple<int, int>>> dic1 = new Dictionary<int, List<Tuple<int, int>>>();
-            int i = 0;
-            foreach(var x in list) {
-                int j = 0;
-                foreach(var y in list){
+            Dictionary<int, Tuple<int, int>> dic1 = new Dictionary<int, Tuple<int, int>>();
+            for(int i=0; i< list.Count(); i++) {
+                var x = list[i];
+                for(int j=0; j< list.Count(); j++){
+                    var y = list[j];
                     var sum = x + y;
-                    var tuples = dic1.GetValueOrDefault(sum);
-                    if (tuples == null) {
-                        dic1.Add(sum, new List<Tuple<int, int>> { new Tuple<int, int>(i, j), new Tuple<int, int>(j, i) });
-                    } else {
-                        tuples.Add(new Tuple<int, int>(i, j));
-                        tuples.Add(new Tuple<int, int>(j, i));
-                        dic1[sum] = tuples;
-                    }
-                    j++;
+                    if (sum == target && i != j) {
+                        if (dic1.ContainsKey(sum)) {
+                            return dic1[target];
+                        } else {
+                            return new Tuple<int, int>(i, j);
+                        }
+                    } else if (!dic1.ContainsKey(sum) && i != j) {
+                        dic1.Add(sum, new Tuple<int, int>(i, j));
+                    } 
                 }          
-                i++;
             }
-            var valores = dic1.GetValueOrDefault(target);
-            if (valores == null) {
-                return null;
-            } else {
-                return valores[0];
-            }
+            return null;
         }
         static void Main(string[] args)
         {          
@@ -39,7 +33,6 @@ namespace Program
                 var item = FindTwoSum(new List<int>() { 3, 1, 5, 7, 5, 9, 4, 8,3,47,85,9,6,25,96,9,332,7,89,62,74,1,78,3,9,3,4,9 }, x);
                 if (item == null) {
                     Console.WriteLine("Não existe");
-
                 } else {
                     Console.WriteLine("chave: ({0}, {1}), soma {2}", item.Item1, item.Item2, x);
                 }
